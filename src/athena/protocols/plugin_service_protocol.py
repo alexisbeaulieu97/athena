325154bd982c5f12981f800253e75ef81bfe1831
Alexis Beaulieu <alexisbeaulieu97@gmail.com>
2025-03-06 00:27:06 -0500

    Enhance PluginServiceProtocol by adding methods for registering single and multiple plugins

--- src/athena/protocols/plugin_service_protocol.py
+++ src/athena/protocols/plugin_service_protocol.py
@@ -1,4 +1,4 @@
-from typing import Generic, Protocol, runtime_checkable
+from typing import Generic, List, Protocol, runtime_checkable
 
 from athena.models.plugin import Plugin
 from athena.types import PluginParametersType, PluginResultType
@@ -6,6 +6,18 @@ from athena.types import PluginParametersType, PluginResultType
 
 @runtime_checkable
 class PluginServiceProtocol(Protocol, Generic[PluginResultType, PluginParametersType]):
+    def register_plugin(
+        self, plugin: Plugin[PluginResultType, PluginParametersType]
+    ) -> None:
+        """Register a plugin."""
+        ...
+
+    def register_plugins(
+        self, plugins: List[Plugin[PluginResultType, PluginParametersType]]
+    ) -> None:
+        """Register multiple plugins."""
+        ...
+
     def get_plugin(
         self, plugin_identifier: str
     ) -> Plugin[PluginResultType, PluginParametersType]:
